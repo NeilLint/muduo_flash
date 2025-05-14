0d6f5a258be4d4596d6326c851c4813d25d04ca7
Lint <lintao.student@icloud.com>
2025-05-14 19:03:04 +0800

    kqv_projection优化_1

--- scripts/compile_read_bin_part_size.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++17 read_bin_part_size.cpp ../src/model/modelConfig.cpp -o read_bin_part_size -I../src
-
-echo "编译完成，使用 ./read_bin_part_size 来运行程序" 
--- scripts/compile_read_model.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-g++ -std=c++17 read_model.cpp ../src/model/modelConfig.cpp -o read_model -I../src
-
-echo "编译完成，使用 ./read_model 来运行程序" 
